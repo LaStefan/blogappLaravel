d3653935b2e4d827797f47a9e74015b836bb6f3f
Stefan Lazarevic <s.lazarevic@student.fontys.nl>
2019-11-19 13:00:00 +0100

    Full CRUD functionality

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="/posts" class="btn btn-primary">Back</a>
+<div class="container">
     <h1>{{$post->title}}</h1>
     <div>
         {{$post->body}}
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <div class="container">
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+    <a href="/posts" class="btn btn-primary">Back</a>
+        <hr>
+        {!!Form::open(['action' => ['PostsController@destroy',$post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+        {{Form::hidden('_method','DELETE')}}
+        {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+        {!!Form::close()!!}
+        </div>
+</div>
 @endsection
