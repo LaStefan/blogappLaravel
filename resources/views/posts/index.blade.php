763025b3453a3d7db67ba085d04953fe99028735
Stefan Lazarevic <s.lazarevic@student.fontys.nl>
2019-11-19 01:48:53 +0100

    Database migrations, eloquent, models and seeding

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Posts</h1>
+    @if(count($posts)>0)
+    <ul class="list-group">
+        @foreach($posts as $post)
+        <li class='list-group-item'>
+            
+            <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
+            <small>Written on {{$post->created_at}}</small>
+            
+        </li>
+        @endforeach
+        
+    </ul>
+    <div class='container'>
+        {{$posts->links()}}
+    </div>
+    
+
+    @else
+    <p>No posts found</p>
+    @endif
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/users/{id}', function ($id) {
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
+
+Route::resource('posts','PostsController');
