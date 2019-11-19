b880001dcbb6ba091bec7896766ffb19a01d9cfd
Stefan Lazarevic <s.lazarevic@student.fontys.nl>
2019-11-19 11:57:30 +0100

    Forms, modeling and CRUD

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -20,5 +20,9 @@
               </li>
            
           </ul>
+          <ul class="nav navbar-nav navbar-right">
+              <li class="nav-item">
+                <a class="nav-link" href="/posts/create">Create Post</a></li>
+          </ul>
         </div>
       </nav>
